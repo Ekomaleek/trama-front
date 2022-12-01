75e0187e48ac9b88ac63496f925dba30774e2df0
Rafael Vieira <rafaellvs322@gmail.com>
2022-12-01 19:34:29 -0300

    ENH: improve theme

--- theme/index.ts
+++ theme/index.ts
@@ -1,5 +1,9 @@
 // https://chakra-ui.com/docs/styled-system/theme
+
 import { robotoSlab, firaSans } from 'pages/_app'
+
+import Heading from './heading'
+import Text from './text'
 
 const colors = {
   primary: '#FFD403',
@@ -10,8 +14,6 @@ const colors = {
   success: '#0af521',
   error: '#ff000c',
 }
-
-const navbarHeight = '80px'
 
 // Global styles from chakra-ui
 const styles = {
@@ -24,18 +26,40 @@ const styles = {
     },
     'h1, h2, h3, h4, h5, h6': {
       fontWeight: 800,
+    },
+    a: {
       fontFamily: firaSans.style.fontFamily,
     },
-    'p, span': {
-      fontWeight: 800,
-      fontFamily: robotoSlab.style.fontFamily,
-    },
   },
+}
+
+const fonts = {
+  heading: firaSans.style.fontFamily,
+  body: robotoSlab.style.fontFamily,
+}
+
+const components = {
+  Heading,
+  Text,
+}
+
+const navbarHeight = '5rem'
+
+const sizes = {
+  navbarHeight,
+}
+
+const space = {
+  navbarHeight,
 }
 
 const theme = {
   colors,
   navbarHeight,
   styles,
+  fonts,
+  components,
+  sizes,
+  space,
 }
 export default theme
