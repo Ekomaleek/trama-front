be62e4da62be5054c790996189c9401b832a1e3f
Rafael Vieira <rafaellvs322@gmail.com>
2022-12-22 07:37:47 -0300

    FEA: Dialog component

---
+++ components/Dialog/index.tsx
@@ -0,0 +1,86 @@
+import { useRef } from 'react'
+
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Heading,
+  Text,
+  Button,
+} from '@chakra-ui/react'
+
+interface DialogProps {
+  isOpen: boolean
+  onClose: () => void
+  isLoading: boolean
+  callback: () => void
+  headerText: string
+  bodyText: string
+  actionBtnText: string
+}
+
+const Dialog = (props: DialogProps): JSX.Element => {
+  const cancelRef = useRef(null)
+
+  const {
+    isOpen,
+    onClose,
+    isLoading,
+    callback,
+    headerText,
+    bodyText,
+    actionBtnText,
+  } = props
+
+  return (
+    <AlertDialog
+      isOpen={isOpen}
+      leastDestructiveRef={cancelRef}
+      onClose={onClose}
+      preserveScrollBarGap={true}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent
+          bg='black'
+          border='1px solid'
+          borderColor='orange.500'
+        >
+          <AlertDialogHeader>
+            <Heading size='lg'>
+              {headerText}
+            </Heading>
+          </AlertDialogHeader>
+
+          <AlertDialogBody>
+            <Text>
+              {bodyText}
+            </Text>
+          </AlertDialogBody>
+
+          <AlertDialogFooter>
+            <Button
+              colorScheme='whiteAlpha'
+              ref={cancelRef}
+              onClick={onClose}
+            >
+              Cancelar
+            </Button>
+            <Button
+              colorScheme='red'
+              onClick={callback}
+              ml={3}
+              isLoading={isLoading}
+            >
+              {actionBtnText}
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  )
+}
+
+export default Dialog
