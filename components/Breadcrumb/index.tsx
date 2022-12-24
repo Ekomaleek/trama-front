b3fd051ec7e65f372ff754362c80c3f52b32d5d6
Rafael Vieira <rafaellvs322@gmail.com>
2022-12-24 03:29:50 -0300

    FEA: add Breadcrumb component

---
+++ components/Breadcrumb/index.tsx
@@ -0,0 +1,53 @@
+import { useRouter } from 'next/router'
+import { getBreadcrumbItems } from 'helpers/breadcrumb'
+
+import NavbarLink from 'components/_core/NavbarLink'
+import {
+  Flex,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Text,
+} from '@chakra-ui/react'
+
+const BreadcrumbComponent = (): JSX.Element => {
+  const router = useRouter()
+  const breadcrumbItems = getBreadcrumbItems(router.asPath)
+
+  return (
+    <Flex
+      mt='navbarHeight'
+      px='8'
+      py='4'
+      alignSelf='flex-start'
+      alignItems='baseline'
+    >
+      <Text mr='4' fontSize='sm'>
+        Você está aqui:
+      </Text>
+      <Breadcrumb
+        separator='>'
+        fontWeight='bold'
+        fontSize='sm'
+      >
+        {breadcrumbItems.map(item =>
+          <BreadcrumbItem key={item.id}>
+            <BreadcrumbLink
+              as={NavbarLink}
+              href={item.url}
+              mr='0'
+              fontSize='sm'
+              _hover={{
+                textDecoration: 'none',
+              }}
+            >
+              {item.name}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        )}
+      </Breadcrumb>
+    </Flex>
+  )
+}
+
+export default BreadcrumbComponent
