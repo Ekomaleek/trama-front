177f52fcebe9015cc9655e2c94d7b9246ab1facb
Rafael Vieira <rafaellvs322@gmail.com>
2022-12-26 10:46:53 -0300

    FEA: add CardRecord component

---
+++ components/Card/Record/index.tsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import NextLink from 'next/link'
+
+import { Record, RecordForDeletion } from 'types/Record'
+
+import { useApi } from 'hooks/use-api'
+import { removeRecord } from 'api/record'
+
+import Dialog from 'components/Dialog'
+import { SettingsIcon, EditIcon, DeleteIcon } from '@chakra-ui/icons'
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  IconButton,
+  useDisclosure,
+} from '@chakra-ui/react'
+
+interface RecordCardProps {
+  record: Record
+  setShouldUpdate: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const RecordCard = ({ record, setShouldUpdate }: RecordCardProps): JSX.Element => {
+  const { isLoading, makeRequest } = useApi<Record, RecordForDeletion>()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const handleDelete = async (): Promise<void> => {
+    await makeRequest({
+      apiMethod: removeRecord,
+      apiMethodArgs: { id: record.id },
+      successMessage: `O registro ${record.name} foi removido com sucesso.`,
+      successCallback: () => setShouldUpdate(true),
+      finallyCallback: () => onClose(),
+    })
+  }
+
+  return (
+    <Card width='300px' height='250px'>
+      <CardHeader>
+        <Heading
+          size='md'
+          flex='1'
+          variant='overflowEllipsis'
+          sx={{ '--n-lines': '1' }}
+        >
+          {record.name}
+        </Heading>
+
+        <Heading size='xs' pt='2'>
+          Categoria: {record.category_id}
+        </Heading>
+      </CardHeader>
+
+      <CardBody>
+        <Text
+          fontSize='sm'
+          variant='overflowEllipsis'
+          sx={{ '--n-lines': '3' }}
+        >
+          {record.description}
+        </Text>
+      </CardBody>
+
+      <CardFooter justifyContent='space-between'>
+        <Button>
+          Ver registro
+        </Button>
+
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            icon={<SettingsIcon />}
+            aria-label='Opções de registro'
+          />
+
+          <MenuList>
+            <NextLink href={`records/update/${record.id}`}>
+              <MenuItem icon={<EditIcon />}>
+                Editar
+              </MenuItem>
+            </NextLink>
+            <MenuItem icon={<DeleteIcon />} onClick={onOpen}>
+              Remover
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </CardFooter>
+
+      <Dialog
+        isOpen={isOpen}
+        onClose={onClose}
+        isLoading={isLoading}
+        callback={handleDelete}
+        headerText='Remover registro'
+        bodyText={`Tem certeza que deseja remover o registro ${record.name}?`}
+        actionBtnText='Remover'
+      />
+    </Card>
+  )
+}
+
+export default RecordCard
