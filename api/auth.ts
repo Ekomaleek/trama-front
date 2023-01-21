c23eab1caefcd763e04285ce4957ef6dc2a53954
Rafael Vieira <rafaellvs322@gmail.com>
2023-01-21 18:09:32 -0300

    FEA: add signup, confirmAccount and resendCode api functions

--- api/auth.ts
+++ api/auth.ts
@@ -1,14 +1,21 @@
 import { customAxios as axios } from './'
 
-import { UserForLogin } from 'types/User'
+import {
+  UserForLogin,
+  UserForSignup,
+  UserFromLogin,
+  UserFromSignup,
+  UserForSignupConfirmation,
+  UserForResendCode,
+} from 'types/User'
 
 import { getErrorMessage } from 'helpers'
 
-const loginUser = async ({ username, password }: UserForLogin): Promise<string> => {
+const loginUser = async (userData: UserForLogin): Promise<UserFromLogin> => {
   try {
     const response = await axios.post(
       '/auth/login',
-      { username, password }
+      { ...userData }
     )
     return response.data
   } catch (err) {
@@ -16,4 +23,49 @@ const loginUser = async ({ username, password }: UserForLogin): Promise<string> 
   }
 }
 
-export { loginUser }
+const signupUser = async (userData: UserForSignup): Promise<UserFromSignup> => {
+  try {
+    const response = await axios.post(
+      '/auth/signup',
+      { ...userData },
+    )
+    return response.data
+  } catch (err) {
+    throw new Error(`Erro ao criar usuário: ${getErrorMessage(err)}`)
+  }
+}
+
+const confirmAccount = async (userData: UserForSignupConfirmation): Promise<string> => {
+  const { username, code } = userData
+
+  try {
+    const response = await axios.post(
+      '/auth/confirm-account',
+      { username, code }
+    )
+    return response.data
+  } catch (err) {
+    throw new Error(`Erro ao confirmar conta: ${getErrorMessage(err)}`)
+  }
+}
+
+const resendConfirmationCode = async (userData: UserForResendCode): Promise<string> => {
+  const { username } = userData
+
+  try {
+    const response = await axios.post(
+      '/auth/resend-code',
+      { username }
+    )
+    return response.data
+  } catch (err) {
+    throw new Error(`Erro ao reenviar o código: ${getErrorMessage(err)}`)
+  }
+}
+
+export {
+  loginUser,
+  signupUser,
+  confirmAccount,
+  resendConfirmationCode,
+}
