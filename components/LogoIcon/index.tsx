eb1a39d9749f05d0e024e1cdd1797608b88392cd
Rafael Vieira <rafaellvs322@gmail.com>
2023-01-25 17:43:43 -0300

    FEA: create LogoIcon component

---
+++ components/LogoIcon/index.tsx
@@ -0,0 +1,26 @@
+import { createIcon } from '@chakra-ui/icons'
+
+const LogoIcon = createIcon({
+  displayName: 'LogoIcon',
+  viewBox: '0 0 100 100',
+  d: `
+    M 25,0 V 100
+    M 50,0 V 100
+    M 75,0 V 100
+    M 0,25 H 100
+    M 0,50 H 100
+    M 0,75 H 100
+  `,
+  defaultProps: {
+    stroke: 'white',
+    strokeWidth: '6',
+    boxSize: '8',
+    transitionProperty: 'stroke',
+    transitionDuration: 'normal',
+    _hover: {
+      stroke: 'orange.500',
+    },
+  },
+})
+
+export default LogoIcon
